873a1ef83953f9c63ba7efa68222034cf6fbb553
Jimmy <b016104b@student.staffs.ac.uk>
2017-06-16 16:18:41 +0100

    missing files

--- HealthApplication/app/src/main/java/com/example/b016104b/healthapplication/Activities/MainActivity.java
+++ HealthApplication/app/src/main/java/com/example/b016104b/healthapplication/Activities/MainActivity.java
@@ -1,15 +1,82 @@
 package com.example.b016104b.healthapplication.Activities;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.b016104b.healthapplication.Fragments.AdventuresFragment;
+import com.example.b016104b.healthapplication.Fragments.ProfileFragment;
+import com.example.b016104b.healthapplication.Fragments.StatisticFragment;
 import com.example.b016104b.healthapplication.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        if (bottomNavigationView != null)
+        {
+            // Set action to perform when any menu-item is selected.
+            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+            {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item)
+                {
+                    selectFragment(item);
+                    return false;
+                }
+            });
+        }
+
+        pushFragment(new ProfileFragment());
     }
+
+
+    protected void selectFragment(MenuItem item) {
+
+        item.setChecked(true);
+
+        switch (item.getItemId()) {
+            case R.id.action_profile:
+                // Action to perform when Home Menu item is selected.
+                pushFragment(new ProfileFragment());
+                break;
+            case R.id.action_adventures:
+                // Action to perform when Bag Menu item is selected.
+                pushFragment(new AdventuresFragment());
+                break;
+            case R.id.action_statistics:
+                // Action to perform when Account Menu item is selected.
+                pushFragment(new StatisticFragment());
+                break;
+        }
+    }
+
+
+    protected void pushFragment(Fragment fragment) {
+        if (fragment == null)
+            return;
+
+        FragmentManager fragmentManager = getFragmentManager();
+        if (fragmentManager != null) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            if (ft != null) {
+                ft.replace(R.id.rootLayout, fragment);
+                ft.commit();
+            }
+        }
+    }
+
+
 }
